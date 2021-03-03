03d4e497f90f80003cfae0a77e1323b0b53a345b
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-03-03 11:34:53 -0300

    RequestResponseDemo

---
+++ toddyjms/src/main/java/toddyjms/messagestructure/RequestResponseDemo.java
@@ -0,0 +1,38 @@
+package toddyjms.messagestructure;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class RequestResponseDemo {
+	public static void main(String[] args) throws Exception {
+		InitialContext context = new InitialContext();
+		Queue requestQueue = (Queue) context.lookup("queue/requestQueue");
+		Queue responseQueue = (Queue) context.lookup("queue/responseQueue");
+
+		try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext()) {
+
+			TextMessage message = jmsContext.createTextMessage("Toddy é o melhor");
+
+			JMSProducer producer = jmsContext.createProducer();
+			message.setJMSReplyTo(responseQueue);
+			producer.send(requestQueue, message);
+
+			JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
+			TextMessage receive = (TextMessage) consumer.receive();
+			System.out.println("Message received: " + receive.getText());
+
+			JMSProducer responseProducer = jmsContext.createProducer();
+			responseProducer.send(responseQueue, "You are awsome!!");
+
+			JMSConsumer responseConsumer = jmsContext.createConsumer(responseQueue);
+			System.out.println("Message received: " + responseConsumer.receiveBody(String.class));
+		}
+	}
+}
